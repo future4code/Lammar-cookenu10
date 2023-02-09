e8d6190c490bd1cc17021e563bf1c1089c834499
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2023-02-09 16:31:01 -0300

    Add Custom error

---
+++ src/error/UserError.ts
@@ -0,0 +1,7 @@
+import { CustomError } from "./CustomError";
+
+// export class NameNotProvided extends CustomError{
+//     constructor(){
+//         super(400, "Provide user name.")
+//     }
+// }
