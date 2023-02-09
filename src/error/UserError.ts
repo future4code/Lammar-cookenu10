fa387c1b6903d339daeeedde7fe72d57c8bc5225
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2023-02-09 18:11:57 -0300

    Add error handling to custom error UserError.ts

--- src/error/UserError.ts
+++ src/error/UserError.ts
@@ -1,7 +1,37 @@
-import { CustomError } from "./CustomError";
+import { CustomError } from "./CustomError"
 
-// export class NameNotProvided extends CustomError{
-//     constructor(){
-//         super(400, "Provide user name.")
-//     }
-// }
+export class NameNotProvided extends CustomError {
+    constructor() {
+        super(400, "Provide user name.")
+    }
+}
+
+export class EmailNotProvided extends CustomError {
+    constructor() {
+        super(400, "Provide email.")
+    }
+}
+
+export class PasswordNotProvided extends CustomError {
+    constructor() {
+        super(400, "Provide password.")
+    }
+}
+
+export class PasswordTooShort extends CustomError {
+    constructor() {
+        super(400, "Passwords must have a minimum of 6 characters.")
+    }
+}
+
+export class InvalidEmail extends CustomError {
+    constructor() {
+        super(400, "Invalid email.")
+    }
+}
+
+export class Unauthorized extends CustomError{ 
+    constructor(){
+        super(401, "User not authorized.")
+    }
+}
