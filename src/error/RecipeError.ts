0b916d1e7f6b840c79c602b69cee3e6102e4cc8c
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2023-02-14 22:17:09 -0300

    Add error handling for Recipe entity

--- src/error/RecipeError.ts
+++ src/error/RecipeError.ts
@@ -5,3 +5,27 @@ export class RecipeNotFound extends CustomError {
         super(404, "Recipe not found.")
     }
 }
+
+export class RecipeTitleNotProvided extends CustomError {
+    constructor() {
+        super(400, "Please inform recipe title.")
+    }
+
+}
+export class RecipeDescriptionNotProvided extends CustomError {
+    constructor() {
+        super(400, "Please inform recipe description.")
+    }
+}
+
+export class RecipeIdNotProvided extends CustomError {
+    constructor() {
+        super(400, "Please inform recipe ID.")
+    }
+}
+
+export class RecipeNotOwned extends CustomError {
+    constructor() {
+        super(400, "Recipe owned by someone else.")
+    }
+}
